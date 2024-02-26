ff73bcf23098b3546a20dac833960b15c19d44e4
Banwo Olorunsogo <sogobanwo@gmail.com>
2024-02-26 16:59:28 +0100

    Feat: Added ScreenShots of the result of the interaction

--- scripts/MainetInteraction.ts
+++ scripts/MainetInteraction.ts
@@ -4,7 +4,9 @@ const helpers = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 const main = async () => {
     const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+
     const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    
     const wethAdress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
 
     const UNIRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
@@ -15,40 +17,22 @@ const main = async () => {
     const impersonatedSigner = await ethers.getSigner(USDCHolder);
 
     const amountOut = ethers.parseUnits("2000", 6);
-    // const amountIn = ethers.parseEther("1");
-    const amountInMax = ethers.parseEther("10")
 
     const USDC = await ethers.getContractAt("IERC20", USDCAddress);
     const DAI = await ethers.getContractAt("IERC20", DAIAddress);
-    const WETH = await ethers.getContractAt("IERC20", wethAdress);
 
     const ROUTER = await ethers.getContractAt("IUniswap", UNIRouter);
 
     const approveTx = await USDC.connect(impersonatedSigner).approve(UNIRouter, amountOut);
     await approveTx.wait();
 
-    const ethBal = await impersonatedSigner.provider.getBalance(USDCHolder);
-    // const wethBal = await WETH.balanceOf(impersonatedSigner.address);
-
     const usdcBal = await USDC.balanceOf(impersonatedSigner.address);
     const daiBal = await DAI.balanceOf(impersonatedSigner.address);
-
-    // console.log("WETH Balance:", ethers.formatUnits(wethBal, 18));
-    // console.log("ETH Balance:",ethers.formatUnits(ethBal, 18));
 
 
     const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
 
-    // const swap = await ROUTER.swapTokensForExactETH(
-    //     amountOut,
-    //     ethers.parseUnits("200",18),
-    //     [USDCAddress, wethAdress],
-    //     impersonatedSigner.address,
-    //     deadline
-    // );
-    // await swap.wait()
-
-    console.log("----------------------------Transaction 1-------------------------------------")
+    console.log("----------------------------Transaction swapExactTokensForTokensSupportingFeeOnTransferTokens-------------------------------------")
 
 
     console.log("USDC Balance:", ethers.formatUnits(usdcBal, 6))
@@ -73,11 +57,11 @@ const main = async () => {
     console.log("dai balance after swap", ethers.formatUnits(daiBalAfterSwapTx1, 18) );
 
 
-    console.log("---------------------------------Transaction 1 Ended--------------------------------")
+    console.log("---------------------------------Transaction swapExactTokensForTokensSupportingFeeOnTransferTokens Ended--------------------------------")
 
 
     // INTERACTION 2
-    console.log("----------------------------Transaction 2-------------------------------------")
+    console.log("----------------------------Transaction swapExactETHForTokens-------------------------------------")
 
 
     const ethBalBeforeTx2 =  await impersonatedSigner.provider.getBalance(USDCHolder);
@@ -114,7 +98,7 @@ const main = async () => {
     console.log("eth balance after swap", ethers.formatUnits(ethBalAfterSwap, 18) );
     console.log("dai balance after swap", ethers.formatUnits(daiBalAfterSwap, 18) );
 
-    console.log("----------------------------Transaction Ended-------------------------------------")
+    console.log("----------------------------Transaction swapExactETHForTokens Ended-------------------------------------")
 
     /*
 
